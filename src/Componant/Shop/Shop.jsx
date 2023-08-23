482d0d29102b90880967afd646a2f60ed19c9857
Naim <mdnaim01303202218@gmail.com>
2023-08-23 21:25:48 +0600

    naim

--- src/Componant/Shop/Shop.jsx
+++ src/Componant/Shop/Shop.jsx
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from "react";
-import './Shop.css'
+import "./Shop.css";
 import Product from "../Product/Product";
 const Shop = () => {
-    const [products, setproducts ] =useState([]);
-    useEffect(()=>{
-        fetch('products.json')
-        .then(res =>res.json())
-        .then(data => setproducts(data))
-    },[])
+  const [products, setproducts] = useState([]);
+  const [cart , setcart] = useState([])
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setproducts(data));
+  }, []);
+  const handelAddtocart = (product) => {
+    // console.log(product);
+    const newCart =[...cart, product]
+    setcart(newCart);
+  };
   return (
     <div className="shop-container">
       <div className="product-container">
         {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
+          <Product
+            key={product.id}
+            product={product}
+            handelAddtocart={handelAddtocart}
+          ></Product>
         ))}
       </div>
       <div className="cart-container">
-        <h1>Order summery </h1>
+        <h1>Order summary </h1>
+        <p>Select items: {cart.length}</p>
       </div>
     </div>
   );
